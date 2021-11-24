7fe9887091e73ac02c1e178d0294a70765bb3b1b
Tom Hung <405040782@gms.tku.edu.tw>
2021-11-25 01:01:48 +0800

    update throttle

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,36 +1,44 @@
 import style from "./style.module.scss";
-import { debounce } from "lodash-es";
+import { debounce, throttle } from "lodash-es";
 import { useRef, useEffect, useState, useCallback, useMemo } from "react";
 
 const SideBar = ({ navs }) => {
   const [scrollY, setScrollY] = useState(0);
   const sideBarRef = useRef(null);
-  const callDebounce = useCallback(
-    debounce((Y) => setScrollY(Y), 200),
+
+  const callThrottle = useCallback(
+    throttle((Y) => setScrollY(Y), 200),
     []
   );
 
-  const NavItem = navs.map((item, index) => {
-    return (
-      <div key={item + index} className={style.item}>
-        {item}
-      </div>
-    );
-  });
-
-  const addScrollListener = () => {
+  const addScrollListener = useCallback(() => {
     window.addEventListener("scroll", () => {
       // 取得 scrollY 使用者垂直滑動的高度
       const Y = window.scrollY;
 
       // 每過 10 毫秒就監測 scrollY 一次
-      debounce((Y) => setScrollY(Y), 10)(Y);
-
-      // scroll 結束後監測 scrollY
-      // 不斷呼叫同一個 useCallback function，debounce 延遲秒數不斷重製直到最後停止才得以執行
-      // callDebounce(Y);
+      callThrottle(Y);
     });
-  };
+  }, []);
+
+  const NavItem = useCallback(
+    navs.map((item, index) => {
+      return (
+        <div key={item + index} className={style.item}>
+          {item}
+        </div>
+      );
+    }),
+    []
+  );
+
+  // const NavItem = navs.map((item, index) => {
+  //   return (
+  //     <div key={item + index} className={style.item}>
+  //       {item}
+  //     </div>
+  //   );
+  // });
 
   useEffect(() => {
     // 監聽頁面的 scroll 事件
@@ -67,12 +75,14 @@ export default SideBar;
 
 // 為什麼 useEffect 中使用 debounce 的差異 ?
 
-// 為何使用 fixed 會異常 ?
+// 為何使用 position:fixed 設定 sideBar 動態 top 屬性會異常 ?
+// - 因為 fixed 是定義當下畫面中與 4 邊的距離，所以當 top 越來越大時自然會不斷向下移動
+// - absolute 是與整個頁面的舉黎
 
 // Memorized Hooks 不用頻繁更新的資料可以用它們進行儲存
 // useMemo vs. useCallBack
 // 回傳值       回傳函式 ( 可帶參數 )
 
-// 如何讓整個頁面 scroll smooth
+// 如何讓整個頁面 scroll smooth ？
 
-// Link to 錨點
+// Link to 錨點 ？
