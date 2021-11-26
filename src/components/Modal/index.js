40d6d728a1b3f9a583b06a9c2c21c02653ca1a43
TW\S441906 <s441906@sinyi.com.tw>
2021-11-26 18:15:18 +0800

    update

---
+++ src/components/Modal/index.js
@@ -0,0 +1,7 @@
+import style from "./style.module.scss";
+
+const Modal = () => {
+  return <div className={style.modal}>This is a Modal</div>;
+};
+
+export default Modal;
--- src/pages/test_DynamicImport.js
+++ src/pages/test_DynamicImport.js
@@ -1,18 +1,21 @@
-import _ from "lodash";
-import { isEmpty, debounce, throttle } from "lodash-es";
+// 即便使用者沒有點擊按鈕也會載入
+import Modal from "../components/Modal";
+import { useState } from "react";
 
 const DynamicImport = () => {
-  const handleClick = () => {
-    isEmpty(null);
-    const Bo = debounce(() => console.log(123), 1000);
-    Bo();
-  };
+  const [display, setDisplay] = useState();
 
   return (
     <div>
-      <div onClick={handleClick}>Click Me !</div>
+      <h1>Title</h1>
+      {/* 這裡可以使用 dynamic import */}
+      {display && <Modal />}
+      <button onClick={() => setDisplay(true)}>Open Modal</button>
+      <p> Page content</p>
     </div>
   );
 };
 
 export default DynamicImport;
+
+// Modal、Drawer ... 等不是每個使用者都會用到的 component 可以使用 dynamic import 簡煥載入過多資源的負擔
